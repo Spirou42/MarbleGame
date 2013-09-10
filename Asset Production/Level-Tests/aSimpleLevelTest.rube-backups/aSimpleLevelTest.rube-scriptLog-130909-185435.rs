3d313b768c7776ea41c97f8410f1803d64ffb6ec
winni <w@T42.de>
2013-09-11 00:10:18 +0200

    new color for bouncy pentagons, different bounciness on the right

--- Asset Production/Level-Tests/aSimpleLevelTest.rube-backups/aSimpleLevelTest.rube-scriptLog-130909-185435.rs
+++
@@ -1,2 +0,0 @@
-//Started script log at Mon Sep  9 18:54:35 2013
-
--- Asset Production/Level-Tests/aSimpleLevelTest.rube-backups/aSimpleLevelTest.rube-scriptLog.rs
+++ Asset Production/Level-Tests/aSimpleLevelTest.rube-backups/aSimpleLevelTest.rube-scriptLog.rs
@@ -1,1 +1,5 @@
-//Restarted script log at Mon Sep  9 20:22:46 2013
+//Restarted script log at Tue Sep 10 22:41:28 2013
+getFixture(9).deselect();
+getFixture(12).select();
+getFixture(12).deselect();
+getFixture(11).select();
